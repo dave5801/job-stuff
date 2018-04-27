ee3a6b1d39f3f74a2d3c534bd1a9e13df3f9093c
David Franklin <dave5801@gmail.com>
2018-04-27 11:00:14 -0700

    using random ints now

--- javascript/how_did_I_hear_about_this_jobs.js
+++ javascript/how_did_I_hear_about_this_jobs.js
@@ -1,3 +1,26 @@
 (function() {
-    console.log("hello again");
+
+    let reasons = {
+                    1: "a wizard told me",
+                    2: "an employee referred me, at least the way I remember it...",
+                    3: "a generic job posting",
+                    4: "a job posting with better font",
+                    5: "I stay often Airbinb"
+                    }
+    
+    function HowIlearnedaboutThisJobMachineFactoryThing(){
+      this.count = 0;
+
+      setInterval(() => {
+        this.count++; // |this| properly refers to the person object
+        
+        let rando = Math.random() * (6 - 1) + 1;
+        Math.floor(rando)
+        console.log("hello again " +Math.floor(rando));
+
+      }, 1000);
+    }
+
+    var stuff = new HowIlearnedaboutThisJobMachineFactoryThing();
+
 }());
