ac41236aba764809d0915a2de86e1e838b6bb761
David Franklin <dave5801@gmail.com>
2018-04-27 09:28:01 -0700

    I am functional

--- what-makes-me-unique.py
+++ what-makes-me-unique.py
@@ -1,10 +1,24 @@
 
 #job: https://jobs.lever.co/rover/bb4dcaeb-b5a9-4332-9c24-e86dc83dcaa3/apply?lever-source=LinkedIn
 
-OTHER_PEOPLE = {}
+OTHER_PEOPLE = {} #dictionary representing the greater human population of the planet Earth
 
-class Me(object '''Don't objectify me!'''):
+#Don't objectify me!
+class Me(object):
+    #I suppose these are the only qualities of a person anyone cares about
     def __init__(self, cool, smart, handsome, awesome):
-        self.cool = cool;
-        self.smart = smart;
-        self.awesome = awesome;
+        self.cool = cool
+        self.smart = smart
+        self.awesome = awesome
+
+    def amIcool(self):
+        return "Hell Yeah...obviously"
+
+    def isThisGuySmart(self):
+        return "I'm so smart applying to a dog walking app -- when I don't even own a dog..."
+
+    def iThinkWeKnowTheAnswerToThisQuestion(self):
+        return "Very Handsome"
+
+    def awesome(self):
+        return ""
