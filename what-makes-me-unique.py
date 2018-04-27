115463992552d4576d1753a9b6ad44589c233f61
David Franklin <dave5801@gmail.com>
2018-04-27 09:40:11 -0700

    I'm undergoing changes, personal development

--- what-makes-me-unique.py
+++ what-makes-me-unique.py
@@ -4,21 +4,43 @@
 OTHER_PEOPLE = {} #dictionary representing the greater human population of the planet Earth
 
 #Don't objectify me!
-class Me(object):
+class AnActualPerson(object):
     #I suppose these are the only qualities of a person anyone cares about
-    def __init__(self, cool, smart, handsome, awesome):
+    def __init__(self, cool=True, smart=True, handsome=True, awesome=True):
         self.cool = cool
         self.smart = smart
+        self.handsome = handsome
         self.awesome = awesome
 
+        #What
     def amIcool(self):
-        return "Hell Yeah...obviously"
-
+        if self.cool == True:
+            return "Hell Yeah...obviously"
+        else:
+            return "Actually I think honesty, hardwork, and desire to learn are what matters, I'm sorry that's not cool"
+        #I
     def isThisGuySmart(self):
-        return "I'm so smart applying to a dog walking app -- when I don't even own a dog..."
-
+        if self.smart == True:
+            return "I'm so smart applying to a dog walking app -- when I don't even own a dog..."
+        else:
+            return "I believe that all humans are intelligent, and its not about how you did in school."
+        #really
     def iThinkWeKnowTheAnswerToThisQuestion(self):
-        return "Very Handsome"
-
+        if self.handsome == True:
+            return "Very Handsome"
+        else:
+            return "If looks are what matters to you -- then I feel sorry for you."
+        #Think
     def awesome(self):
-        return ""
+        if self.awesome == True:
+            return "Chuck Norris, Unicorns, and Lazer Beam Explosions!!!!"
+        else:
+            return "Sorry, but I'm just as cool or awesome as most normal people in this world"
+
+
+if __name__ == '__main__':
+    David = AnActualPerson()
+    print(David.amIcool())
+    print(David.isThisGuySmart())
+    print(David.iThinkWeKnowTheAnswerToThisQuestion())
+    print(David.awesome())
