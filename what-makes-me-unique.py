7cae57c0c2e66088cc6089f68d0e174b04ff891d
David Franklin <dave5801@gmail.com>
2018-04-27 09:22:16 -0700

    i am initialized, as in me, myself

---
+++ what-makes-me-unique.py
@@ -0,0 +1,10 @@
+
+#job: https://jobs.lever.co/rover/bb4dcaeb-b5a9-4332-9c24-e86dc83dcaa3/apply?lever-source=LinkedIn
+
+OTHER_PEOPLE = {}
+
+class Me(object '''Don't objectify me!'''):
+    def __init__(self, cool, smart, handsome, awesome):
+        self.cool = cool;
+        self.smart = smart;
+        self.awesome = awesome;
