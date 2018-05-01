9598fc648201b1fe4da1568f353b0b2e8b5de80a
David Franklin <dave5801@gmail.com>
2018-05-01 09:49:43 -0700

    geez, I just can't seem to get away from the master branch

---
+++ encrypt-this/Caesar.java
@@ -0,0 +1,18 @@
+class Caesar{
+
+    static String andYouBrutus(String andYou, int idesOfMarch){
+        String thenFallCaesar = "";
+
+        System.out.println((int)andYou.charAt(0));
+       // int x = Math.abs((int)str.charAt(i);
+        return "";
+    }
+
+    public static void main(String[] args){
+       
+
+        String toEncrypt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int rotate = 23;
+        andYouBrutus(toEncrypt, rotate);
+    }
+}
