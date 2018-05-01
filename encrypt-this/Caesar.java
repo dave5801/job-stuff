5fa65169b0e0ed4b9f1d5a21c8e7aec792e9edf1
David Franklin <dave5801@gmail.com>
2018-05-01 11:35:12 -0700

    with comments

--- encrypt-this/Caesar.java
+++ encrypt-this/Caesar.java
@@ -1,17 +1,23 @@
 class Caesar{
 
+    //look up: https://en.wikipedia.org/wiki/Caesar_cipher
+
+    //takes in plaintext and a rotation
     static String andYouBrutus(String andYou, int idesOfMarch){
         StringBuilder thenFallCaesar=new StringBuilder("");  
   
         int wouldCassiuslikeASCIIvalues = 0;
 
+        //loop plaintext
         for(int senators = 0; senators < andYou.length(); senators++){
 
             wouldCassiuslikeASCIIvalues = (int)andYou.charAt(senators);
 
+            //check for uppercase.
             if (Character.isUpperCase(andYou.charAt(senators))){
                 char constantAsTheNorthStar = (char)((wouldCassiuslikeASCIIvalues+idesOfMarch - 65)%26 + 65);
                 thenFallCaesar.append(constantAsTheNorthStar);
+            //check lowercase.
             }else{
                 char constantAsTheNorthStar = (char)((wouldCassiuslikeASCIIvalues+idesOfMarch - 97)%26 + 97);
                 thenFallCaesar.append(constantAsTheNorthStar);
@@ -28,5 +34,6 @@ class Caesar{
         int rotate = 23;
         String encrypted = andYouBrutus(toEncrypt, rotate);
         System.out.println(encrypted);
+        System.out.println("Historically, Julius Caesar was a pioneer in the field of infosec.");
     }
 }
