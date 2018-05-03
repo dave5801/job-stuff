2f2de5d096bca1d4f457a5873c7d1a2924fcf706
David Franklin <dave5801@gmail.com>
2018-05-03 08:08:51 -0700

    equating java to religion, because I'm being strange

---
+++ java/to_be_wierd/StaticBeliefs.java
@@ -0,0 +1,26 @@
+class DynamicRebel{
+
+    //i am open to new ideas
+
+    //but not beyond core beliefs
+    final static String beliefs = "I am true to myself";
+
+    String process1;
+    String process2;
+
+    //I can accept new ideas
+    protected DynamicRebel(String process1, String process2){
+        this.process1 = process1;
+        this.process2 = process2;
+    }
+}
+
+
+class StaticBeliefs{
+
+    final static String dogma = "I believe in only this; all others are the enemy";
+
+    public static void main(String args[]){
+        System.out.println("I am unchanging in my beliefs");
+    }
+}
