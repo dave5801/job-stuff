b9373b50d66d0716632beef6746109c7cdd87ecb
David Franklin <dave5801@gmail.com>
2018-05-05 13:34:58 -0700

    saving my progress, could zombies be real?

--- javascript/weather_portents.js
+++ javascript/weather_portents.js
@@ -1,6 +1,6 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var HttpClient = function() {
+var MegatronTheUnholyClient = function() {
     this.get = function(theUnspeakableUrl, aCallback) {
         var sacrificialHttpRequest = new XMLHttpRequest();
         sacrificialHttpRequest.onreadystatechange = function() { 
@@ -13,13 +13,34 @@ var HttpClient = function() {
     }
 }
 
-var theClientIsTheEvilPriest = new HttpClient(); 
+var theClientIsTheEvilPriest = new MegatronTheUnholyClient(); 
 
 var iSummonTheEvilMachineGods = "http://samples.openweathermap.org/data/2.5/forecast?q=Seattle,DE&appid=b6907d289e10d714a6e88b30761fae22";
 
+var theMechanicalOverLordSpeaksinRiddles = {};
+
 theClientIsTheEvilPriest.get(iSummonTheEvilMachineGods, function(andTheyAnswer) {
     // this program is a reflection of my own madness, and despair
-    var theMechanicalOverLordSpeaksinRiddles = JSON.parse(andTheyAnswer);
-    console.log(theMechanicalOverLordSpeaksinRiddles);
-    
+    theMechanicalOverLordSpeaksinRiddles = JSON.parse(andTheyAnswer);
+
+    var darkWhispersOfTheWeather = theMechanicalOverLordSpeaksinRiddles;
+    //console.log(darkWhispersOfTheWeather.list[0].main.humidity); //--> DON"T DELETE
+    console.log(typeof(darkWhispersOfTheWeather.list));
+    console.log(Object.keys(darkWhispersOfTheWeather.list).length);
+
+   
+
 });
+
+
+
+/*
+I need 
+City, State
+
+Temp, in Degrees
+Sky Conditions
+Humidity, by percent --> probably in "main": ...
+
+
+*/
